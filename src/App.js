9d6b32a74198c04ed5e63495e8464a31507464cc
Ostyn Sy <osy304@insite.4cd.edu>
2022-03-15 18:06:21 -0700

    move images and finished navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import './App.css';
+import './style/App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  );
+	return (
+		<Router>
+			<Navbar>
+				<Routes>
+					<Route path='/' exact></Route>
+				</Routes>
+			</Navbar>
+		</Router>
+	);
 }
 
 export default App;
+
+<div className='app'></div>;
